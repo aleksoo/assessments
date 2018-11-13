4e7a83659c7c2d0c43308fde52a6da07452700a3
Aleksander <a.walecki95@gmail.com>
2018-11-13 15:21:23 +0100

    Dziala cozacko

--- Equal_XO.c
+++ Equal_XO.c
@@ -1,11 +1,24 @@
 #include <string.h>
 #include <assert.h>
+#include <stdio.h>
+#include "stdbool.h"
 
 /**
 * Function checks if a string has the same amount of 'x' and 'o's. Function must be case insensitive.
 */
 bool XO(const char *word, const int length)
 {
+	int i = 0, xCount = 0, oCount = 0;
+
+	// Iterujemy po otrzymanym slowie
+	for(i=0; i<length; ++i){
+		// Jezeli X lub O wystepuje, to zwiekszamy liczniki
+		if(word[i]=='x' || word[i]=='X') xCount++;
+		if(word[i]=='o' || word[i]=='O') oCount++;
+	}
+
+	if(xCount == oCount) return true;
+	else return false;
 
 }
 
