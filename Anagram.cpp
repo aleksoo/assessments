5f6f25665e8cdc0e40c316c57e1a91817f50f525
Aleksander <a.walecki95@gmail.com>
2018-11-13 14:56:56 +0100

    Wyklada sie przy takim czyms jak nie znajdzie litery

--- Anagram.cpp
+++ Anagram.cpp
@@ -1,5 +1,7 @@
 #include <assert.h>
 #include <string>
+#include <iostream>
+#include <algorithm>
 
 /** 
 * Function returns true if word_1 and word_2 are anagrams to each other. Otherwise false.
@@ -7,7 +9,22 @@
 */
 bool check_if_anagram(const std::string &word_1, const std::string &word_2)
 {
-	return false;
+	std::cout << "Word_1 = " << word_1 << ", Word_2 = " << word_2 << std::endl;
+	std::string temp = word_2;
+	//temp.erase(std::remove(temp.begin(), temp.end(), ' ') , temp.end());
+	std::cout << "String2: " <<  temp << std::endl;
+
+	
+	for(char singleLetter1 : word_1){
+		std::cout << singleLetter1 << std::endl;
+		temp.erase(temp.find(singleLetter1), 1);		
+	}
+
+	temp.erase(std::remove(temp.begin(), temp.end(), ' ') , temp.end());
+	std::cout << "Test: " << temp << ", Size: " << temp.size() << std::endl;
+
+	if(temp.size()) return false;
+	else return true;
 }
 
 void test_cases()
@@ -21,7 +38,7 @@ void test_cases()
 	answer = check_if_anagram("funeral", "real fun");
 	assert(answer == true);
 
-	answer = check_if_anagram("Tieto", "Tietonator",);
+	answer = check_if_anagram("Tieto", "Tietonator");
 	assert(answer == false);
 
 	answer = check_if_anagram("Football", "Basketball");
