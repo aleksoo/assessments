baa8e916a02abbc749e0e01a6b88b9616ac3e9c9
Aleksander <a.walecki95@gmail.com>
2018-11-14 14:55:44 +0100

    zmiany w anagramie plus to co zrobilem z zadaniem C

--- Inkubator_Development/Assessment_Check/Part_1/Anagram.cpp
+++ Inkubator_Development/Assessment_Check/Part_1/Anagram.cpp
@@ -11,23 +11,32 @@ bool check_if_anagram(const std::string &word_1, const std::string &word_2)
 {
 
 	// Sprawdzamy czy ktorys ze stringow nie jest pusty
-	if(!(word_1.size() && word_2.size())) return false;
+	if(word_1.empty() || word_2.empty()) 
+		return false;
 
-	// Tworzymy kopie word_2
+	// Tworzymy kopie word_1 i word_2
+	std::string word_1_copy = word_1;
+	std::transform(word_1_copy.begin(), word_1_copy.end(), word_1_copy.begin(), ::tolower);
 	std::string word_2_copy = word_2;
-
-	// Iterujemy przez litery word_1 i usuwamy z tymczasowego napotkane litery
-	for(char singleLetter1 : word_1){
-		int place = word_2_copy.find(singleLetter1);
-		if(place+1) word_2_copy.erase(place, 1);		
-	}
+	std::transform(word_2_copy.begin(), word_2_copy.end(), word_2_copy.begin(), ::tolower);
 
 	// Usuwamy z kopii drugiego slowa spacje
 	word_2_copy.erase(std::remove(word_2_copy.begin(), word_2_copy.end(), ' ') , word_2_copy.end());
 
+	// Iterujemy przez litery word_1 i usuwamy z tymczasowego napotkane litery
+	for(char singleLetter1 : word_1_copy){
+		int place = word_2_copy.find(singleLetter1);
+		if(place+1) 
+			word_2_copy.erase(place, 1);		
+	}
+
+	
+
 	// Jezeli zostaly nam jakies litery to znaczy, ze nie jest to anagram
-	if(word_2_copy.size()) return false;
-	else return true;
+	if(word_2_copy.size()) 
+		return false;
+	else 
+		return true;
 }
 
 void test_cases()
@@ -49,6 +58,9 @@ void test_cases()
 
 	answer = check_if_anagram("F", "");
 	assert(answer == false);
+
+	answer = check_if_anagram("Buckethead", "DeathCubeK");
+	assert(answer == true);
 }
 
 int main()
