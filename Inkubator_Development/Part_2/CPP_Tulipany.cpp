cfad81ed0241f8aed4b48321bcf6c9d42271d297
Aleksander <a.walecki95@gmail.com>
2018-11-16 16:21:23 +0100

    Created files

---
+++ Inkubator_Development/Part_2/CPP_Tulipany.cpp
@@ -0,0 +1,14 @@
+#include <assert.h>
+#include <iostream>
+
+void test_cases();
+
+int main(int argc, char *argv[])
+{
+	test_cases();
+	return 0;
+}
+
+void test_cases(){
+    
+}
---
+++ Inkubator_Development/Part_2/C_Pierwszy_i_Ostatni.c
@@ -0,0 +1,13 @@
+#include <assert.h>
+
+void test_cases();
+
+int main(int argc, char *argv[])
+{
+	test_cases();
+	return 0;
+}
+
+void test_cases(){
+    
+}
