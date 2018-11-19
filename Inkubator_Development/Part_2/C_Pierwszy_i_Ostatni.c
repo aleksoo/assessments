e928893271f0dd3148448acb485888b8b9d6e84e
Aleksander <a.walecki95@gmail.com>
2018-11-19 14:44:52 +0100

    Saving

--- Inkubator_Development/Part_2/C_Pierwszy_i_Ostatni.c
+++ Inkubator_Development/Part_2/C_Pierwszy_i_Ostatni.c
@@ -1,13 +1,15 @@
 #include <assert.h>
 
-void test_cases();
+void test_cases(){
+    // assert(cos && cos)
+}
+
+int* lowest_and_highest(){
+
+}
 
 int main(int argc, char *argv[])
 {
 	test_cases();
 	return 0;
-}
-
-void test_cases(){
-    
 }
