105da3e9547d307a62a53fdaa68776b1c467d436
Aleksander <a.walecki95@gmail.com>
2018-11-19 13:57:57 +0100

    [Code Review] First Review]

--- Inkubator_Development/Part_2/CPP_Tulipany.cpp
+++ Inkubator_Development/Part_2/CPP_Tulipany.cpp
@@ -18,74 +18,59 @@ void test_cases(){
 	int answer = 0;
 	std::stringstream inputStream;
 
-
 	inputStream << "8\n3 6 2 2 4 6 3 7";
 	answer = count_tulips(inputStream);
 	assert(answer == 14995);
 	inputStream.str("");
 	inputStream.clear();
 
-	std::string valuesString = "15000\n";
-	for(int i = 0; i < 15000 ; ++i){
-		valuesString += std::to_string(i) + ' ';
-	}
-	inputStream << valuesString;
+	inputStream << "2\n1 15000";
+	answer = count_tulips(inputStream);
+	assert(answer == 14998);
 	inputStream.str("");
 	inputStream.clear();
 
+	inputStream << "1\n15001";
 	answer = count_tulips(inputStream);
-	assert(answer == 14995);
+	assert(answer == 15000);
+	inputStream.str("");
+	inputStream.clear();
 
-    
+	std::string valuesString = "15000\n";
+	for(int i = 1; i <= 15000 ; ++i){
+		valuesString += std::to_string(i) + ' ';
+	}
+	inputStream << valuesString;
+	answer = count_tulips(inputStream);
+	assert(answer == 0);
+	inputStream.str("");
+	inputStream.clear();    
 }
 
-int count_tulips(std::stringstream& inputStream){
-	
+int count_tulips(std::stringstream& inputStream){	
 	std::array<bool, 15000> ownedTulips;
 	std::fill(ownedTulips.begin(), ownedTulips.end(), 0);
-	int rows, tulip, missingTulips = 0, numberOfTulips = 0;
+	int rows = 0, tulip = 0, missingTulips = 0, numberOfTulips = 0;
 	std::string::size_type sz;
 	std::string inputString;
 	
 	getline(inputStream, inputString);
-	std::cout << inputString << std::endl;
-	rows = stoi(inputString, &sz);
-	// std::cout << rows << std::endl;
-	// std::srand(NULL);
+	rows = stoi(inputString);
 
-	// do{		
-	// 	std::cin >> rows;
-	// } while( !(rows > 0 && rows < 20000) );
+	if(rows == 0)
+		return 15000;
 
-	// wypelnianie wartosci
 	for(int i = 0; i < rows; ++i){
 			getline(inputStream, inputString, ' ');
 			tulip = stoi(inputString, &sz);
-			if( tulip > 0 && tulip < 15000 )
+			if( tulip > 0 && tulip <= 15000 )
 				ownedTulips[tulip] = true;
 	}
-	
-	// // int placeholderRows = rand() % 5000;
-	// std::vector<int> tulipVector; 
-	// for(int i = 0; i < placeholderRows; ++i){
-	// 	tulipVector.push_back(rand() % 15000);
-	// }
 
-	// for(int i = 0; i < placeholderRows; ++i){
-	// 	if(!ownedTulips[ tulipVector[i] ]){
-	// 		ownedTulips[ tulipVector[i] ] = true;
-	// 	}
-	// }
-
-	for(int i = 0; i < 15000; ++i){
-		if(!ownedTulips[i])
+	for(int i = 1; i <= 15000; ++i){
+		if( !ownedTulips[i] )
 			++missingTulips;
 	}
 
-// for(int i = 0; i < 15000; ++i){
-// 	std::cout << ownedTulips[i] << ' ';
-// }
-
-	std::cout << "Liczba brakujacych tulipanow: " << missingTulips << std::endl;
 	return missingTulips;
 }
