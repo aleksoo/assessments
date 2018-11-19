cbb9e467520e8d074c970b14ecc4547cba159f72
aleksoo <a.walecki95@gmail.com>
2018-11-20 00:01:44 +0100

    Development

--- Inkubator_Development/Part_2/C_Pierwszy_i_Ostatni.c
+++ Inkubator_Development/Part_2/C_Pierwszy_i_Ostatni.c
@@ -2,41 +2,81 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
-void lowest_and_highest(char *inputString){
-	int interval[2];
-	int returnInterval[2] = {-1, -1};
-	int *days;
+int* lowest_and_highest(char *inputString){
+	bool flag = true;
+	int days;
+	long price;
+	static int daysInterval[2] = {-1, -1};
+	long *valueOfTheDay;
+	int size = 0;
 	char *token;
-	char* tempString = (char*)malloc(strlen(inputString) * sizeof(char));
+	char *tempString = (char*)malloc(strlen(inputString) * sizeof(char));
     strcpy(tempString, inputString);
 
-	
+	token = strtok(tempString, " \n");
+	days = strtol(token, NULL, 10);
+	if( days < 1 || days > 100000 )
+		return daysInterval;
 
-	token = strtok(tempString, " ");
-	interval[0] = strtol(token, NULL, 10);
-	token = strtok(tempString, "\n");
-	interval[1] = strtol(token, NULL, 10);
-	
-	
+	token = strtok(NULL, "\n");
+	price = strtol(token, NULL, 10);	
+	if( price < 1 || price > 1000000000 )
+		return daysInterval;
 
-	//TO NA WPROWADZONE DNI
-	// while(token != NULL){
+	token = strtok(NULL, " \n");
+	while(token != NULL){		
+		++size;
+		if(size <= days){
+			valueOfTheDay = realloc(valueOfTheDay, size * sizeof(long));
+			*(valueOfTheDay + size - 1) = strtol(token, NULL, 10); // Subtracting 1 to keep index of int array correct.		
+		}
+		token = strtok(NULL, " \n");
+	}
 
-	// 	token = strtok(tempString, " \n");
-	// }
+	// PRZEITEROWAC PRZEZ DAYS
+	for(int i = 0; i < size; ++i){
+		if(*(valueOfTheDay + i) == price){
+			printf("codochuja %ld %ld\n", *(valueOfTheDay + i), price);
+			daysInterval[1] = i + 1; // Added 1 to get correct day.
+			if(flag){
+				daysInterval[0] = i + 1;
+				flag = false;
+			}			
+		}			
+	}
 
-	//return &przedzial;
+	free(token);
+	free(tempString);
+	return daysInterval;
 }
 
 void test_cases(){
 	char *inputString = NULL;
-	int *answer;
+	int *answer = malloc( 2 * sizeof(int));
+
+	inputString = "0 3\n1 3 5 2 3 4 3";
+	answer = lowest_and_highest(inputString);
+	assert( answer[0] == -1 && answer[1] == -1 );
 	
-	inputString = "7 3\n1 3 5 3 4 3";
-	lowest_and_highest(inputString);
-	// answer = lowest_and_highest(inputString);
-    // assert( answer[0] == 2 && answer[1] == 7 );
+	inputString = "7 3\n1 3 5 2 3 4 3";
+	answer = lowest_and_highest(inputString);
+	//printf("%d %d\n", answer[0], answer[1]);
+    assert( answer[0] == 2 && answer[1] == 7 );
+
+	inputString = "4 2\n 2 3 4 5";
+	answer = lowest_and_highest(inputString);
+	//printf("%d %d\n", answer[0], answer[1]);
+    assert( answer[0] == 1 && answer[1] == 1 );
+
+	inputString = "3 2\n 5 3 1";
+	answer = lowest_and_highest(inputString);
+	printf("%d %d\n", answer[0], answer[1]);
+    assert( answer[0] == -1 && answer[1] == -1 );
+
+	free(inputString);
+	free(answer);
 }
 
 
