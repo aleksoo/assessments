acd44b1d592e41ccb1f766ebab5689e319b9b2cc
Aleksander <a.walecki95@gmail.com>
2018-11-26 16:42:03 +0100

    last commit before home

--- Inkubator_Development/Part_4/CPP_RomanNumerals.cpp
+++ Inkubator_Development/Part_4/CPP_RomanNumerals.cpp
@@ -5,26 +5,45 @@
 #include <cmath>
 #include <map>
 
-std::map<int, std::string> intToRoman = { {1, "I"}, {4, "IV"}, {5, "V"}, {9, "IX"},
-                                            {10, "X"}, {40, "XL"}, {50, "L"}, {90, "XC"},
-                                            {100, "C"}, {400, "CD"}, {500, "D"}, {900, "CM"}, {1000, "M"} };
+
 
 std::string romanNumberals(int inputNumber){
+    static std::map<int, std::string> intToRoman = { {1, "I"}, {4, "IV"}, {5, "V"}, {9, "IX"},
+                                            {10, "X"}, {40, "XL"}, {50, "L"}, {90, "XC"},
+                                            {100, "C"}, {400, "CD"}, {500, "D"}, {900, "CM"}, {1000, "M"} };
     std::string roman;
     std::vector<int> singleDigits;
     int digitsCounter =  std::to_string(inputNumber).length();
 
-    for(int i = digitsCounter - 1; i >= 0; --i){ // Subtracting 1 to keep index correct.
-        int numberToInsert = (int)(inputNumber / pow(10, i)) % 10;
-        singleDigits.insert(singleDigits.begin(), i);
-    }
-
-    
+    while(inputNumber > 0)
+        for(auto iter = intToRoman.rbegin(); iter != intToRoman.rend(); ++iter){
+            if(inputNumber / iter->first){
+                roman += iter->second;    
+                inputNumber -= iter->first;
+                break;
+            } 
+        }
     return roman;
 }
 
 void test_cases(){
-    romanNumberals(1243);
+    std::string test = romanNumberals(1000);
+    assert(test == "M");
+ 
+    test = romanNumberals(1990);
+    assert(test == "MCMXC");
+ 
+    test = romanNumberals(2008);
+    assert(test == "MMVIII");
+ 
+    test = romanNumberals(1666);
+    assert(test == "MDCLXVI");
+ 
+    test = romanNumberals(1);
+    assert(test == "I");
+ 
+    test = romanNumberals(0);
+    assert(test == "");
 }
 
 int main(int argc, char *argv[])
