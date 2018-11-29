14c33f6d8f8af2d79183bb84013c65ef8b9ae3d1
Aleksander <a.walecki95@gmail.com>
2018-11-29 12:54:47 +0100

    jump error in valgrind

--- Inkubator_Development/Part_5/printf_function.c
+++ Inkubator_Development/Part_5/printf_function.c
@@ -7,29 +7,32 @@
 char *mr_asprintf(const char* inputString, ...){
     va_list args;
     va_start(args, inputString);
-    int i = 0, j = 0, k = 0;
-    char buff[100] = {0}, tmp[20];
+    int i = 0, j = 0, bufferSize = 100;
+    char buff[100] = {'\0'}, tmp[20];
+    memset(buff, '\0', bufferSize*sizeof(char));
     
 
     while(inputString && inputString[i]){
-        if(inputString[i] == '{' && inputString[i + 2] == '}'){
+        if(inputString[i] == '\0')
+            break;
+
+        if(inputString[i] == '{'){
             ++i;
-            switch(inputString[i]){ // na koniec dodaje tego bracketa w kazdym wypadku
-                case 'i': // czemu nie dzialaja dwa argumenty?
+            switch(inputString[i]){
+                case 'i':
                     snprintf(tmp, 20, "%d", va_arg(args, int));
                     strncpy(&buff[j], tmp, strlen(tmp));
                     j += strlen(tmp);
-                    ++j;
+                    i += 2;
                     break;
-                case 's': // 
+                case 's': 
                     snprintf(tmp, 20, "%s", va_arg(args, char*));
                     strncpy(&buff[j], tmp, strlen(tmp));
                     j += strlen(tmp);
-                    k = 0;
+                    i += 2;
                     break;
                 case '{':
-                    snprintf(tmp, 1, "%s", va_arg(args, char*));
-                    strncpy(&buff[j], tmp, sizeof(char));
+                    buff[j] = inputString[i];
                     ++j;
                     ++i;
                     break;
@@ -37,11 +40,12 @@ char *mr_asprintf(const char* inputString, ...){
         } else {
             buff[j] = inputString[i];
             ++j;
+            ++i;
         }
-        ++i;
+        
     }
     va_end(args);
-    char *returnString = (char*)malloc( strlen(buff) * sizeof(char));
+    char *returnString = (char*)malloc( (strlen(buff) + 1)* sizeof(char*));
     strncpy(returnString, buff, strlen(buff));
     printf("TEST: %s\n", returnString);
     return returnString;
@@ -49,35 +53,32 @@ char *mr_asprintf(const char* inputString, ...){
 
 void test_cases(){
     char* test = NULL;
-    // test = mr_asprintf("Gaius Julius Caesar Augustus Germanicus");
-    // printf("%s\n", test);
-    // assert(!strcmp(test, "Gaius Julius Caesar Augustus Germanicus"));
-    // free(test);
 
-    // test = mr_asprintf("Nickname: {s}", "Caligula");
-    // printf("%s\n", test);
-    // assert(!strcmp(test, "Nickname: Caligula"));
-    // free(test);
+    test = mr_asprintf("Gaius Julius Caesar Augustus Germanicus");
+    printf("%s\n", test);
+    assert(!strcmp(test, "Gaius Julius Caesar Augustus Germanicus"));
+    free(test);
+    
 
-    // test = mr_asprintf("Number: {i} {i}", 10, 20);
-    // printf("%s\n", test);
-    // assert(!strcmp(test, "Number: 10 20"));
-    // free(test);
+    test = mr_asprintf("Nickname: {s}", "Caligula");
+    printf("%s\n", test);
+    assert(!strcmp(test, "Nickname: Caligula"));
+    free(test);
 
-    // test = mr_asprintf("Number: {i ");
-    // printf("%s\n", test);
-    // assert(!strcmp(test, "Number: {i"));
-    // free(test);
+    test = mr_asprintf("N: {i} {i} ", 10, 20);
+    printf("%s\n", test);
+    assert(!strcmp(test, "N: 10 20 "));
+    free(test);
 
-    // test = mr_asprintf("Bracket: {{");
-    // printf("%s\n", test);
-    // assert(!strcmp(test, "Bracket: {"));
-    // free(test);
+    test = mr_asprintf("Literal: {s}", "c");
+    printf("%s\n", test);
+    assert(!strcmp(test, "Literal: c"));
+    free(test);
 
-    // test = mr_asprintf("Literal: {s}", 'c');
-    // printf("%s\n", test);
-    // assert(!strcmp(test, "Literal: c"));
-    // free(test);
+    test = mr_asprintf("Bracket: {{ abcd");
+    printf("%s\n", test);
+    assert(!strcmp(test, "Bracket: { abcd"));
+    free(test);
 
 }
 
